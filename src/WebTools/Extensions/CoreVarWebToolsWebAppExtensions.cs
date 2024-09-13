fa49199df8ae34b93faccf851dcdfe52d41fbed9
Jeff Scherrer <jeff@mushmo.com>
2024-09-13 13:38:32 -0700

    Added hostname redirection.

---
+++ src/WebTools/Extensions/CoreVarWebToolsWebAppExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Builder;
+
+public static class CoreVarWebToolsWebAppExtensions
+{
+
+	public static WebApplication UseHostnameRedirection(this WebApplication app, string targetHostname)
+	{
+		app.Use(async (context, next) =>
+		{
+			if (context.Request.Host.Host.Equals(targetHostname, StringComparison.OrdinalIgnoreCase))
+				await next();
+			else
+				context.Response.Redirect(string.Concat(context.Request.Scheme, "://", targetHostname, context.Request.Path, context.Request.QueryString.Value), true, true);
+		});
+
+		return app;
+	}
+
+	public static WebApplication UseHostnameRedirection(this WebApplication app, string targetHostname, params string[] matchingHostnames)
+	{
+		var redirectableHostnames = new HashSet<string>(matchingHostnames, StringComparer.OrdinalIgnoreCase);
+		app.Use(async (context, next) =>
+		{
+			if (!redirectableHostnames.Contains(context.Request.Host.Host) || context.Request.Host.Host.Equals(targetHostname, StringComparison.OrdinalIgnoreCase))
+				await next();
+			else
+				context.Response.Redirect(string.Concat(context.Request.Scheme, "://", targetHostname, context.Request.Path, context.Request.QueryString.Value), true, true);
+		});
+
+		return app;
+	}
+
+	public static WebApplication UseHttpsHostnameRedirection(this WebApplication app, string targetHostname)
+	{
+		app.Use(async (context, next) =>
+		{
+			if (context.Request.Host.Host.Equals(targetHostname, StringComparison.OrdinalIgnoreCase))
+				await next();
+			else
+				context.Response.Redirect(string.Concat("https://", targetHostname, context.Request.Path, context.Request.QueryString.Value), true, true);
+		});
+
+		return app;
+	}
+
+	public static WebApplication UseHttpsHostnameRedirection(this WebApplication app, string targetHostname, params string[] matchingHostnames)
+	{
+		var redirectableHostnames = new HashSet<string>(matchingHostnames, StringComparer.OrdinalIgnoreCase);
+		app.Use(async (context, next) =>
+		{
+			if (!redirectableHostnames.Contains(context.Request.Host.Host) || context.Request.Host.Host.Equals(targetHostname, StringComparison.OrdinalIgnoreCase))
+				await next();
+			else
+				context.Response.Redirect(string.Concat("https://", targetHostname, context.Request.Path, context.Request.QueryString.Value), true, true);
+		});
+
+		return app;
+	}
+
+}
